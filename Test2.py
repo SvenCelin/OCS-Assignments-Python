f27b0451821a859fde8e53266de8b23dec686a37
Mauro Jurada <37582356+MauroJurada@users.noreply.github.com>
2019-10-29 12:33:50 +0100

    first

---
+++ Test2.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy as sp
+from mpl_toolkits.mplot3d import axes3d, Axes3D #<-- Note the capitalization! 
+
+def g(x):
+    return (x**2. * np.sin(20*x) + np.cos(x-np.pi))**3 + 1
+
+#1
+def f(x, y):
+    return (2*x**3. - 6*y**2 + 3*y*x**2)
+
+x = np.linspace(-1, 1, 100)
+y = np.linspace(-1, 1, 100)
+
+X, Y = np.meshgrid(x, y)
+Z = f(X, Y)
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.contour3D(X, Y, Z, 50, cmap='binary')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+ax.set_title('3D contour')
+plt.show()
+#plt.plot(x, g(x))
+#plt.show()
+
