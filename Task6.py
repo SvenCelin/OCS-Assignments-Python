f1f9e63e1d16a3a0a8aebb6464e38813f0380a86
Mivancic01 <m.ivancic@student.tugraz.at>
2019-12-10 15:25:37 +0100

    6. zadatak s krivin armijo i calc_d_Loss

--- Task6.py
+++ Task6.py
@@ -4,6 +4,7 @@ from scipy.optimize import approx_fprime
 import matplotlib.pyplot as plt
 
 import Task4
+import Task5
 dtype = np.float64
 
 nabla_f = lambda x: np.where(x > 1, 3*x/2 + 1/2, np.where(x < -1, 3*x/2 - 1/2, 2*x))
@@ -30,24 +31,61 @@ def armijo(K, x_k):
     return t_k
         #update_plots(k, x_k, t_k)
 
-def steepestDescent(x_k, K, t, d, constantStep):
+def steepestDescent(x_k, K, d, constantStep):
     print("x_k shape: ", x_k.shape)
     print("d shape: ", d.shape)
+    t = 0.01
     for k in range(K):
-        x_k += t * d
         if(constantStep == False):
             t = armijo(K, x_k)
+        x_k += t * d
     return x_k
 
-def train(x, W, b, act, y):
+def calcD_Loss(inY, resY, N, S):
+    sum = 0
+    for i in S:
+        for j in N:
+            if(resY[i][j] == 0):
+                print("DIVIDE BY ZERO!!!!")
+            else:
+                sum += inY[i][j]/np.log(resY[i][j])
+    return sum/S
+    
+def deriveP(inY, resY, S, W, b, d_act, a):
+    # compute errors e in reversed order
+    print("BACK PROPAGATION")
+    assert(len(a) == len(resY))
+    e = [None] * len(a)
+    e[-1] = d_act[-1](resY[-1]) * calcD_Loss(inY, resY, 4, S) # delta_L
+    for l in range(len(a) - 2, 0, -1):
+        e[l] = d_act[l-1](resY[l]) * (W[l].T @ e[l+1])
+            
+    # compute gradient for W an b
+    dW = [None] * len(a)
+    db = [None] * len(a)
+    for l in range(len(a) - 1):
+        dW[l] = np.outer(e[l+1], a[l])
+        db[l] = e[l+1]
+        
+    return dW, db
+
+def train(x, W, b, y, K, constantStep):
     inputY = [ [ 0 for i in range(4) ] for j in range(len(y)) ] 
     i = 0
     for elem in y:
         inputY[i][elem] = 1
         i+=1
     
-    temp, resultY = Task4.feed_forward(x, W, b, act)
-    
+    activations = [Task4.lnAct, Task4.softMax]
+    a, resultY = Task4.feed_forward(x, W, b, activations)
+
+    d_act = [Task5.d_lnAct, Task5.d_softmax]
+    dW, db = deriveP(inputY, resultY, len(y), W, b, d_act, a)
+
+    W[0] = steepestDescent(W[0], K, dW[0], False)
+    W[1] = steepestDescent(W[1], K, dW[1], False)
+    b[0] = steepestDescent(b[0], K, db[0], False)
+    b[1] = steepestDescent(b[1], K, db[1], False)
 
 def compareError(S, armijoErr, standardErr):
     sum = 0
