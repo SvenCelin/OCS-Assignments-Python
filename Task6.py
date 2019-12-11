a2f5beed040fa19ee41a9ed93bb0c6890380f38c
Mivancic01 <m.ivancic@student.tugraz.at>
2019-12-11 13:09:46 +0100

    push, push, PUSH! Its a Boy :D

--- Task6.py
+++ Task6.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.optimize import approx_fprime
+from numpy import linalg as LA
 
 import matplotlib.pyplot as plt
 
@@ -22,28 +23,27 @@ def armijo(K, x_k, pos, x, W, b, activations, inY):
     a, resY = Task4.feed_forward(x, W, b, activations)
 
     
-    loss = Loss(inY, resY, 4, len(resY))
-    d_loss = dLoss(inY, resY, 4, len(resY))
+    loss = Loss(inY, resY)
+    d_loss = dLoss(inY, resY)
 
     for k in range(K):
-        grad = nabla_f(x_k)
         for m_k in range(10):
              t_k = s*beta**m_k
              if(pos == 0):
-                 W[0] = x_k - t_k*grad
+                 W[0] = x_k - t_k*d_loss
              if(pos == 1):
-                 W[1] = x_k - t_k*grad
+                 W[1] = x_k - t_k*d_loss
              if(pos == 2):
-                 b[0] = x_k - t_k*grad
+                 b[0] = x_k - t_k*d_loss
              if(pos == 3):
-                 b[1] = x_k - t_k*grad
+                 b[1] = x_k - t_k*d_loss
 
              a, newResY = Task4.feed_forward(x, W, b, activations)
-             newLoss = Loss(inY, resY, 4, len(resY))
+             newLoss = Loss(inY, resY)
 
-             if  loss - newLoss >= sigma*t_k*(d_loss**2).sum():
+             if  LA.norm(loss - newLoss) >= LA.norm(sigma*t_k*(d_loss**2)):
                  break
-        x_k -= t_k * grad
+        x_k -= t_k * d_loss
 
     return t_k
         #update_plots(k, x_k, t_k)
@@ -58,7 +58,19 @@ def steepestDescent(x_k, K, d, constantStep, pos, x, W, b, activations, inY):
         x_k += t * d
     return x_k
 
+def Loss(y,y_tilde):
 
+    print("Loss, y SHAPE: ", np.asarray(y).shape)
+    print("Loss, y_tilde SHAPE: ", np.asarray(y_tilde).shape)
+    return -(y*np.log(y_tilde))
+    
+def dLoss(y,y_tilde):
+
+    print("DLoss, y SHAPE: ", np.asarray(y).shape)
+    print("DLoss, y_tilde SHAPE: ", np.asarray(y_tilde).shape)
+    return -(y/y_tilde)
+
+    """
 def Loss(inY, resY, N, S):
     sum = 0
     for i in S:
@@ -78,7 +90,7 @@ def deriveP(inY, resY, S, W, b, d_act, a):
     print("BACK PROPAGATION")
     assert(len(a) == len(resY))
     e = [None] * len(a)
-    e[-1] = d_act[-1](resY[-1]) * dLoss(inY, resY, 4, S) # delta_L
+    e[-1] = d_act[-1](z[-1]) @ d_loss(y, a[-1]) # delta_L
     for l in range(len(a) - 2, 0, -1):
         e[l] = d_act[l-1](resY[l]) * (W[l].T @ e[l+1])
             
@@ -90,30 +102,72 @@ def deriveP(inY, resY, S, W, b, d_act, a):
         db[l] = e[l+1]
         
     return dW, db
+    """
 
 def train(x, W, b, y, K, constantStep):
     
-    
+    inW0 = W[0]
+    inW1 = W[1]
+    inb0 = b[0]
+    inb1 = b[1]
+    sumConstant = 0
+    sumArmijo = 0
+    for i in range(len(y)):
+        activations = [Task4.lnAct, Task4.softMax]
+        a, resultY = Task4.feed_forward(x, W, b, activations)
+
+        d_act = [Task5.d_lnAct, Task5.d_softmax]
+        dW, db, e = Task5.back_prop(y[i], W, b, d_act, a, resultY)
+
+        W_constant0 = steepestDescent(inW0, K, dW[0], False, 0, x, W, b, activations, y[i])
+        W_constant1 = steepestDescent(inW1, K, dW[1], False, 1, x, W, b, activations, y[i])
+        b_constant0 = steepestDescent(inb0, K, db[0], False, 2, x, W, b, activations, y[i])
+        b_constant1 = steepestDescent(inb1, K, db[1], False, 3, x, W, b, activations, y[i])
+
+        W_armijo0 = steepestDescent(inW0, K, dW[0], True, 0, x, W, b, activations, y[i])
+        W_armijo1 = steepestDescent(inW1, K, dW[1], True, 1, x, W, b, activations, y[i])
+        b_armijo0 = steepestDescent(inb0, K, db[0], True, 2, x, W, b, activations, y[i])
+        b_armijo1 = steepestDescent(inb1, K, db[1], True, 3, x, W, b, activations, y[i])
+
+        W[0] = W_constant0
+        W[1] = W_constant1
+        b[0] = b_constant0
+        b[1] = b_constant1
+        
+        a, resultY = Task4.feed_forward(x, W, b, activations)
+        sumConstant += compareError(y[i], resultY)
+
+        W[0] = W_armijo0
+        W[1] = W_armijo1
+        b[0] = b_armijo0
+        b[1] = b_constant1
+        
+        a, resultY = Task4.feed_forward(x, W, b, activations)
+        sumArmijo += compareError(y[i], resultY)
+
+    sumConstant /= len(y)
+    sumArmijo /= len(y)
+        """
     activations = [Task4.lnAct, Task4.softMax]
     a, resultY = Task4.feed_forward(x, W, b, activations)
 
     d_act = [Task5.d_lnAct, Task5.d_softmax]
-    dW, db = deriveP(inputY, resultY, len(y), W, b, d_act, a)
+    dW, db, e = Task5.back_prop(y[0], W, b, d_act, a, resultY)
 
 
-    W_constant0 = steepestDescent(W[0], K, dW[0], False, 0, x, W, b, activations, inputY)
-    W_constant1 = steepestDescent(W[1], K, dW[1], False, 1, x, W, b, activations, inputY)
-    b_constant0 = steepestDescent(b[0], K, db[0], False, 2, x, W, b, activations, inputY)
-    b_constant1 = steepestDescent(b[1], K, db[1], False, 3, x, W, b, activations, inputY)
+    W_constant0 = steepestDescent(W[0], K, dW[0], False, 0, x, W, b, activations, y)
+    W_constant1 = steepestDescent(W[1], K, dW[1], False, 1, x, W, b, activations, y)
+    b_constant0 = steepestDescent(b[0], K, db[0], False, 2, x, W, b, activations, y)
+    b_constant1 = steepestDescent(b[1], K, db[1], False, 3, x, W, b, activations, y)
 
-    W_armijo0 = steepestDescent(W[0], K, dW[0], True, 0, x, W, b, activations, inputY)
-    W_armijo1 = steepestDescent(W[1], K, dW[1], True, 1, x, W, b, activations, inputY)
-    b_armijo0 = steepestDescent(b[0], K, db[0], True, 2, x, W, b, activations, inputY)
-    b_armijo1 = steepestDescent(b[1], K, db[1], True, 3, x, W, b, activations, inputY)
+    W_armijo0 = steepestDescent(W[0], K, dW[0], True, 0, x, W, b, activations, y)
+    W_armijo1 = steepestDescent(W[1], K, dW[1], True, 1, x, W, b, activations, y)
+    b_armijo0 = steepestDescent(b[0], K, db[0], True, 2, x, W, b, activations, y)
+    b_armijo1 = steepestDescent(b[1], K, db[1], True, 3, x, W, b, activations, y)
+    """
 
-def compareError(S, armijoErr, standardErr):
-    sum = 0
-    for s in S:
-        if(armijoErr[s] == standardErr[s]):
-            sum+=1
-    return sum/S
+def compareError(y, y_tilde):
+    if(y == y_tilde):
+        return 1
+    else:
+        return 0
