737f93c916850444121b9e0c975f94d64a6ee98b
gabrielaozegovic <gabriela.ozegovic@gmail.com>
2019-12-11 15:50:15 +0100

    graph

--- Ass2_main.py
+++ Ass2_main.py
@@ -60,27 +60,34 @@ if __name__ == '__main__':
 
     w = 400 
     h = 4
-    x_x = [[0 for x in range(w)] for y in range(h)]
-    x_y = [[0 for x in range(w)] for y in range(h)]
-    x_z = [[0 for x in range(w)] for y in range(h)]
+    #x_x = [[0 for x in range(w)] for y in range(h)]
+    #x_y = [[0 for x in range(w)] for y in range(h)]
+    #x_z = [[0 for x in range(w)] for y in range(h)]
+
+    x_x = [None] * len(x_test)
+    x_y = [None] * len(x_test)
+    x_z = [None] * len(x_test)
 
     for i in range (0, len(x_test), 1):
-        if(y_train[i] == 0):
-            x_x[0][i] = x_test[i][0]
-            x_y[0][i] = x_test[i][1]
-            x_z[0][i] = x_test[i][2]
-        if(y_train[i] == 1):
-            x_x[1][i] = x_test[i][0]
-            x_y[1][i] = x_test[i][1]
-            x_z[1][i] = x_test[i][2]
-        if(y_train[i] == 2):
-            x_x[2][i] = x_test[i][0]
-            x_y[2][i] = x_test[i][1]
-            x_z[2][i] = x_test[i][2]
-        if(y_train[i] == 3):
-            x_x[3][i] = x_test[i][0]
-            x_y[3][i] = x_test[i][1]
-            x_z[3][i] = x_test[i][2]
+        x_x[i] = x_test[i][0]
+        x_y[i] = x_test[i][1]
+        x_z[i] = x_test[i][2]
+        #if(y_train[i] == 0):
+        #    x_x[0][i] = x_test[i][0]
+        #    x_y[0][i] = x_test[i][1]
+        #    x_z[0][i] = x_test[i][2]
+        #if(y_train[i] == 1):
+        #    x_x[1][i] = x_test[i][0]
+        #    x_y[1][i] = x_test[i][1]
+        #    x_z[1][i] = x_test[i][2]
+        #if(y_train[i] == 2):
+        #    x_x[2][i] = x_test[i][0]
+        #    x_y[2][i] = x_test[i][1]
+        #    x_z[2][i] = x_test[i][2]
+        #if(y_train[i] == 3):
+        #    x_x[3][i] = x_test[i][0]
+        #    x_y[3][i] = x_test[i][1]
+        #    x_z[3][i] = x_test[i][2]
 
 
     print("X_X", x_x)
@@ -89,14 +96,46 @@ if __name__ == '__main__':
     print("\n\n")
     print("X_Z", x_z)
 
+    acolor = [None] * len(y_test)
+    for i in range (0, len(y_test), 1):
+        if(y_test[i] == 0):
+            acolor[i] = "red"
+        if(y_test[i] == 1):
+            acolor[i] = "blue"
+        if(y_test[i] == 2):
+            acolor[i] = "green"
+        if(y_test[i] == 3):
+            acolor[i] = "yellow"
+
+    y_test2 = y_test
+    amarker = [None] * len(y_test)
+    for i in range (0, len(y_test), 1):
+        if(y_test[i] == y_test2[i]):
+            amarker[i] = '+'
+        if(y_test[i] != y_test2[i]):
+            amarker[i] = '_'
+
+    markerPlus = [None] * len(y_test)
+    markerMinus = [None] * len(y_test)
+
+
     fig = plt.figure()
     ax = plt.axes(projection='3d')
-    ax.scatter(x_x[0], x_y[0], x_z[0], cmap='viridis', linewidth=0.5)
-    ax.scatter(x_x[1], x_y[1], x_z[1], c= 'red', cmap='viridis', linewidth=0.5)
-    ax.scatter(x_x[2], x_y[2], x_z[2], c= 'green', cmap='viridis', linewidth=0.5)
-    ax.scatter(x_x[3], x_y[3], x_z[3], c= 'yellow', cmap='viridis', linewidth=0.5)
+    #ax.scatter(x_x[0], x_y[0], x_z[0], cmap='viridis', linewidth=0.5)
+    #ax.scatter(x_x[1], x_y[1], x_z[1], c= 'red', cmap='viridis', linewidth=0.5)
+    #ax.scatter(x_x[2], x_y[2], x_z[2], c= 'green', cmap='viridis', linewidth=0.5)
+    #ax.scatter(x_x[3], x_y[3], x_z[3], c= 'yellow', cmap='viridis', linewidth=0.5)
+    #ax.scatter(x_x, x_y, x_z, c=acolor,  cmap='viridis', linewidth=0.5)
+    #ax.scatter(x_x, x_y, x_z, c='black', marker = '+',  cmap='viridis', linewidth=0.5)
+    
+    string = [None] * 4
+    string[0] = "y = 0"
+    string[1] = "y = 1" 
+    ax.scatter(x_x, x_y, x_z, c=acolor, cmap='viridis', linewidth=0.5)
+    for i in range(len(y_test)):
+        ax.scatter(x_x[i], x_y[i], x_z[i], c="black", marker=amarker[i], cmap='viridis', linewidth=0.5)
+    ax.legend()
     plt.show()
-
 
     """
     W_array = np.array(W)
